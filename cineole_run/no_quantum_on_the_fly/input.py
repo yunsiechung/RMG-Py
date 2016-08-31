69dad0d9e620cb6eb505a0b4224eafa24374efd9
yunsie <yunsie@mit.edu>
2016-08-31 15:54:10 -0400

    created cineole run input files
    one with no on-the-fly quantum calculation
    and 3 with on-the-fly quantum calculations using
    3 different mopac methods: pm3, pm6, and pm7

---
+++ cineole_run/no_quantum_on_the_fly/input.py
@@ -0,0 +1,99 @@
+database(
+    thermoLibraries = ['KlippensteinH2O2','primaryThermoLibrary','CBS_QB3_1dHR', 'DFT_QCI_thermo', 'GRI-Mech3.0','Cineole'],
+    reactionLibraries = [('Cineole',False)],
+    seedMechanisms = ['KlippensteinH2O2'],
+    kineticsDepositories = ['training'],
+    kineticsFamilies = 'default',
+    kineticsEstimator = 'rate rules',
+)
+
+generatedSpeciesConstraints(
+    allowed = [],
+    maximumCarbonAtoms = 10,
+    maximumOxygenAtoms = 5,
+    maximumRadicalElectrons = 2,
+    allowSingletO2 = False,
+)
+
+species(
+    label='Cineole',
+    reactive=True,
+    structure=SMILES("C12(CCC(C(C)(C)O2)CC1)C"),
+)
+
+species(
+    label='O2',
+    reactive=True,
+    structure=SMILES("[O][O]"),
+)
+
+species(
+    label='Cl',
+    reactive=False,
+    structure=SMILES("[Cl]"),
+)
+
+species(
+    label='HCl',
+    reactive=False,
+    structure=SMILES("Cl"),
+)
+
+
+species(
+    label='He',
+    reactive=False,
+    structure=SMILES("[He]"),
+)
+
+species(
+    label='Cl2',
+    reactive=False,
+    structure=SMILES("ClCl"),
+)
+
+simpleReactor(
+    temperature=(550,'K'),
+    pressure=(4,'torr'),
+    initialMoleFractions={
+        "Cineole": 3.7e-4,
+        "Cl2": 8.4e-4,
+        "Cl": 2.565e-5,
+        "O2": 2.56e-1,
+        "He": 7.42e-1,
+    },
+    terminationTime=(1e-3,'s'),
+)
+
+simpleReactor(
+    temperature=(650,'K'),
+    pressure=(4,'torr'),
+    initialMoleFractions={
+        "Cineole": 3.7e-4,
+        "Cl2": 8.41e-4,
+        "Cl": 2.52e-5,
+        "O2": 2.52e-1,
+        "He": 7.46e-1,
+    },
+    terminationTime=(1e-3,'s'),
+)
+
+
+simulator(
+    atol=1e-16,
+    rtol=1e-8,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=0.01,
+    toleranceInterruptSimulation=0.01,
+    maximumEdgeSpecies=1000000
+)
+
+options(
+    units='si',
+    saveRestartPeriod=(2,'hour'),
+    generateOutputHTML=False,
+    generatePlots=False,
+)
