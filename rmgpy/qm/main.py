b516c878aca22086f7a8521eac40b7ce813a9c7c
yunsie <yunsie@mit.edu>
2016-09-06 17:15:24 -0400

    added pm6-d3 as an additional method for mopac
    pm6-d3 method contains Grimme's corrections for dispersion

--- rmgpy/qm/main.py
+++ rmgpy/qm/main.py
@@ -211,6 +211,8 @@ class QMCalculator():
                 qm_molecule_calculator = rmgpy.qm.mopac.MopacMolPM6(molecule, self.settings)
             elif self.settings.method == 'pm7':
                 qm_molecule_calculator = rmgpy.qm.mopac.MopacMolPM7(molecule, self.settings)
+            elif self.settings.method == 'pm6-d3':
+                qm_molecule_calculator = rmgpy.qm.mopac.MopacMolPM6D3(molecule, self.settings)
             else:
                 raise Exception("Unknown QM method '{0}' for mopac".format(self.settings.method))
             thermo0 = qm_molecule_calculator.generateThermoData()
